a81d992a903a9dea4c3ff4268645bc873d9ad466
Greg V <greg@unrelenting.technology>
2016-03-17 02:03:28 +0300

    fix

--- library/Data/IndieWeb/SiloToMicroformats.hs
+++ library/Data/IndieWeb/SiloToMicroformats.hs
@@ -10,7 +10,6 @@ import           Data.Aeson.Lens (key)
 import           Data.Monoid
 import           Data.Maybe
 import           Control.Applicative
-import           Data.Default -- XXX
 import           Data.Microformats2.Parser.HtmlUtil
 import           Data.Microformats2.Parser.Property (getImgSrc)
 import           Data.Microformats2.Parser (extractProperty, Mf2ParserSettings)
@@ -38,12 +37,12 @@ parseTwitter s r = if isJust tweet && isJust username && isJust tweetText
         comments = mapMaybe (parseTwitter s) $ r ^.. hasClass "permalink-replies" ./ hasClass "tweet"
         profileUrl (Just x) = String $ "https://twitter.com/" <> T.dropWhile (== '@') x
         profileUrl _ = Null
-        tweetText = tweet >>= (^? hasClass "tweet-text") >>= return . extractProperty def "e"
+        tweetText = tweet >>= (^? hasClass "tweet-text") >>= return . extractProperty s "e"
         fullname = tweet >>= (^? hasClass "fullname") >>= getInnerTextRaw
         timestamp = tweet >>= (^? hasClass "_timestamp" . attr "data-time")
         username = tweet >>= (^? hasClass "username") >>= getInnerTextRaw
         avatar = tweet >>= (^? hasClass "avatar") >>= getImgSrc
-        permalink = tweet >>= (^? hasClass "tweet-timestamp") >>= return . extractProperty def "u"
+        permalink = tweet >>= (^? hasClass "tweet-timestamp") >>= return . extractProperty s "u"
         tweet = r ^? hasClass "permalink-tweet" <|> r ^? hasClass "tweet"
         hasClass c = entire . attributeSatisfies "class" (c `T.isInfixOf`)
         str (Just x) = String x
